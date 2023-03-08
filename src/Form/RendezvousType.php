35700fa48fd19c2d58e2802a7381efd49a03a82f
chiheb <chiheb.menjli1920@gmail.com>
2023-03-08 22:33:32 +0100

    Dhia

---
+++ src/Form/RendezvousType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Collecte;
+use App\Entity\Rendezvous;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RendezvousType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateRV')
+            ->add('adresseRV')
+            ->add('etatRV')
+
+            ->add('idcollecte' , EntityType::class, [
+                'label' => 'collecte',
+                'class' => Collecte::class,
+                'choice_label' => 'id',
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Rendezvous::class,
+        ]);
+    }
+}
